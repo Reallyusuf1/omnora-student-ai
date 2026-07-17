7eaa777faeaf201ce05afd4177b937f398fcff5c
Reallyusuf1 <149268526+Reallyusuf1@users.noreply.github.com>
2026-07-17 08:59:17 +0100

    Fix SUPABASE_ANON_KEY syntax and add alerts
    Fixed syntax error in SUPABASE_ANON_KEY assignment and added alerts for debugging.

--- js/supabase.js/supabase.js
+++ js/supabase.js/supabase.js
@@ -3,14 +3,19 @@
 // SUPABASE CLIENT
 // ==========================================
 
+alert("Supabase JS loaded");
+
+alert(typeof window.supabase);
+
 const SUPABASE_URL = "https://qnheojayfgtdohmezxju.supabase.co";
 
-const SUPABASE_ANON_KEY = "sb_publishable_xds1jJt0bjywElP-9tN_sg_TUBC6zgT";
+const SUPABASE_ANON_KEY =  = "sb_publishable_xds1jJt0bjywElP-9tN_sg_TUBC6zgT";
 
 const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
 );
 
 window.supabaseClient = supabase;
 
+alert(typeof window.supabaseClient);
