0967c02df8449fd730a45e4637da8ca5c9810146
Reallyusuf1 <149268526+Reallyusuf1@users.noreply.github.com>
2026-07-17 10:09:41 +0100

    Implement error handling for Supabase SDK
    Added error handling for Supabase SDK loading.

--- js/supabase.js/supabase.js
+++ js/supabase.js/supabase.js
@@ -11,6 +11,10 @@ const SUPABASE_URL = "https://qnheojayfgtdohmezxju.supabase.co";
 
 const SUPABASE_ANON_KEY =  = "sb_publishable_xds1jJt0bjywElP-9tN_sg_TUBC6zgT";
 
+if (!window.supabase) {
+    alert("Supabase SDK not loaded");
+    throw new Error("Supabase SDK not loaded");
+}
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
